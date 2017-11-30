c320a58106800ba61fe5ff5cdd2e17828c50bf2c
MINT <penpitcha.nati@gmail.com>
2017-11-30 15:15:25 +0700

    remove fixie auth

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
-$proxy = 'http://fixie:uJnH2Iy3A5mERc2@velodrome.usefixie.com:80';
-$proxyauth = 'http://fixie:uJnH2Iy3A5mERc2@velodrome.usefixie.com:80';
+// $proxy = 'http://fixie:uJnH2Iy3A5mERc2@velodrome.usefixie.com:80';
+// $proxyauth = 'http://fixie:uJnH2Iy3A5mERc2@velodrome.usefixie.com:80';
 
 $access_token = 'y5dQuFSNT2nKdOR7tZRyuivJag56imvnuD33E+6nKDTQQsQnGgW/fp+czUAGLzfD7Gx+tv5kJ+7DfJZHKfQAuS051Uw2f9JaFAJe1+z+AkudSmoR4kIdSXLBk04CALuJAOrFpMpoAD0ET3NibpNBhQdB04t89/1O/w1cDnyilFU=';
 
@@ -40,8 +40,8 @@ if (!is_null($events['events'])) {
    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-   curl_setopt($ch, CURLOPT_PROXY, $proxy);
-   curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+//    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+//    curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
    $result = curl_exec($ch);
    curl_close($ch);
 
--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
-$proxy = 'http://fixie:uJnH2Iy3A5mERc2@velodrome.usefixie.com:80';
-$proxyauth = 'http://fixie:uJnH2Iy3A5mERc2@velodrome.usefixie.com:80';
+// $proxy = 'http://fixie:uJnH2Iy3A5mERc2@velodrome.usefixie.com:80';
+// $proxyauth = 'http://fixie:uJnH2Iy3A5mERc2@velodrome.usefixie.com:80';
 
 $access_token = 'y5dQuFSNT2nKdOR7tZRyuivJag56imvnuD33E+6nKDTQQsQnGgW/fp+czUAGLzfD7Gx+tv5kJ+7DfJZHKfQAuS051Uw2f9JaFAJe1+z+AkudSmoR4kIdSXLBk04CALuJAOrFpMpoAD0ET3NibpNBhQdB04t89/1O/w1cDnyilFU=';
 
@@ -12,8 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+// curl_setopt($ch, CURLOPT_PROXY, $proxy);
+// curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
